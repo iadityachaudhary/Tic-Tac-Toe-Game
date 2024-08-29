3c433c3c7f94a15dd94b5b2d42251c7f4d274648
Aditya Chaudhary <iadityachaudhary26112003@gmail.com>
2024-08-29 12:16:17 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,95 @@
+let boxes=document.querySelectorAll(".box");
+let resetBtn=document.querySelector("#reset");
+
+let newGamebtn=document.querySelector("#new-game");
+let message=document.querySelector(".msgContainer");
+let msg=document.querySelector("#msg");
+
+
+let turnO = true;
+
+const winPatterns=[
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,5,6],
+    [3,4,5],
+    [6,7,8]
+];
+let clickCount=0;
+
+const resetGame=()=>{
+    turnO=true;
+    enableBoxes();
+    message.classList.add("hide");
+    clickCount=0;
+}
+
+boxes.forEach((box)=>{
+    box.addEventListener("click",()=>{
+        console.log("Box was clicked");
+        clickCount++;
+        if(turnO){
+            box.innerText="O";
+            box.style.color="#0077b6";
+            turnO=false;
+        }else{
+            box.innerText="X";
+            box.style.color="#e63946";
+            turnO=true;
+        }
+        box.disabled=true; 
+
+        checkWinner();
+    })
+})
+
+const disableBoxes=() =>{
+    for(let box of boxes){
+        box.disabled=true;
+    }
+}
+const enableBoxes=() =>{
+    for(let box of boxes){
+        box.disabled=false;
+        box.innerText="";
+    }
+}
+
+const showWinner=(winner)=>{
+    msg.innerText=`Congratulations , Winner is ${winner}`;
+    message.classList.remove("hide");
+    disableBoxes();
+}
+
+const showDraw=()=>{
+    msg.innerText='No Winner Found (Match Draw)';
+    message.classList.remove("hide");
+}
+
+const checkWinner=() => {
+    let winnerFound=false;
+    for(let pattern of winPatterns){
+        let pos1=boxes[pattern[0]].innerText;
+        let pos2=boxes[pattern[1]].innerText;
+        let pos3=boxes[pattern[2]].innerText;
+
+        if(pos1!="" && pos2!="" && pos3!=""){
+            if(pos1==pos2 && pos2==pos3){
+                console.log("Winner:",pos1);
+                showWinner(pos1);
+                winnerFound=true;
+                break;
+            }
+        }
+        if (!winnerFound && clickCount === 9){
+            showDraw();
+        }
+    }
+}
+
+newGamebtn.addEventListener("click",resetGame);
+resetBtn.addEventListener("click",resetGame);
+
