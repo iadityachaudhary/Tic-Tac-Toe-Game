328ebf29f8df89f7ce56c0f8836fb5392c027f64
Aditya Chaudhary <iadityachaudhary26112003@gmail.com>
2024-08-29 13:52:23 +0530

    2nd commit

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const winPatterns=[
     [0,4,8],
     [1,4,7],
     [2,5,8],
-    [2,5,6],
+    [2,4,6],
     [3,4,5],
     [6,7,8]
 ];
@@ -69,26 +69,25 @@ const showDraw=()=>{
     message.classList.remove("hide");
 }
 
-const checkWinner=() => {
-    let winnerFound=false;
-    for(let pattern of winPatterns){
-        let pos1=boxes[pattern[0]].innerText;
-        let pos2=boxes[pattern[1]].innerText;
-        let pos3=boxes[pattern[2]].innerText;
-
-        if(pos1!="" && pos2!="" && pos3!=""){
-            if(pos1==pos2 && pos2==pos3){
-                console.log("Winner:",pos1);
-                showWinner(pos1);
-                winnerFound=true;
-                break;
-            }
+const checkWinner = () => {
+    for (let pattern of winPatterns) {
+      let pos1 = boxes[pattern[0]].innerText;
+      let pos2 = boxes[pattern[1]].innerText;
+      let pos3 = boxes[pattern[2]].innerText;
+      if (pos1 != "" && pos2 != "" && pos3 != "") {
+        if (pos1 == pos2 && pos2 == pos3) {
+          console.log("Winner:", pos1);
+          showWinner(pos1);
+          return; 
         }
-        if (!winnerFound && clickCount === 9){
-            showDraw();
-        }
+      }
     }
-}
+    
+    if (clickCount === 9) {
+      showDraw();
+    }
+  }
+
 
 newGamebtn.addEventListener("click",resetGame);
 resetBtn.addEventListener("click",resetGame);
